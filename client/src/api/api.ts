86f4d4c3b7ff54a2cd651712521b1797ece2011d
Aldrin <acifliku21@epoka.edu.al>
2025-10-28 22:55:22 +0100

    Added the api handler in frontend

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,12 +1,47 @@
+import axios, { AxiosInstance, AxiosResponse } from "axios";
 import { BACKEND_URL } from "@/lib/constants";
 
 export class Api {
+  private axiosInstance: AxiosInstance;
 
-    private baseUrl = BACKEND_URL;
+  constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: BACKEND_URL,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true,
+    });
 
-    async get(path: string) {}
-    async put(path: string, body: any) {}
-    async post(path: string, body: any) {}
-    async delete(path: string, body: any) {}
+    // Global 401 handler
+    this.axiosInstance.interceptors.response.use(
+      (response: AxiosResponse) => response,
+      (error) => {
+        if (error.response && error.response.status === 401) {
+          // TODO: handle 401
+        }
+        return Promise.reject(error);
+      }
+    );
+  }
 
-};
+  async get(path: string, params?: any) {
+    const res = await this.axiosInstance.get(path, { params });
+    return res.data;
+  }
+
+  async post(path: string, body?: any) {
+    const res = await this.axiosInstance.post(path, body);
+    return res.data;
+  }
+
+  async put(path: string, body?: any) {
+    const res = await this.axiosInstance.put(path, body);
+    return res.data;
+  }
+
+  async delete(path: string, body?: any) {
+    const res = await this.axiosInstance.delete(path, { data: body });
+    return res.data;
+  }
+}
